54da230b22053b4233ddb179e78f91b64040b0a1
riyeonlee <gjffod100@gmail.com>
2023-09-22 06:32:42 +0900

    upload

--- todolistP/ProfilePageVC.swift
+++ todolistP/ProfilePageVC.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+
+
+
+class ProfilePageViewController : UIViewController{
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    
+    }
+    
+    
+    
+    
+    
+}
+
